28383ceed20f25098d52a8a934e362c3a5205403
Michael Frank <maickomtutos@gmail.com>
2021-12-22 10:18:27 -0300

    Feito colapse

--- lib/presentation/pages/dashboard/dashboard_page.dart
+++ lib/presentation/pages/dashboard/dashboard_page.dart
@@ -57,171 +57,195 @@ class DashboardPage extends GetView<DashboardController> {
                         // ),
                         withSpaceHeight(size: Get.height.percent(10)),
                         withSpaceHeight(size: 10),
-                        Container(
-                          padding: EdgeInsets.all(Layout.defaultPadding),
-                          width: Get.width,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                                Layout.defaultBorderRadius),
-                            color: Colors.white.withOpacity(0.1),
+                        ExpansionTile(
+                          title: Text(
+                            '',
+                            style: TextStyle(color: Colors.white),
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Obx(
-                                () => HeaderTableWidget(
-                                  title: 'PGE',
-                                  isShowPdf: true,
-                                  onSearch: (value) =>
-                                      controller.searchCompany(value),
-                                  data: controller
-                                          .companyModelSearchRx.value.isEmpty
-                                      // ignore: invalid_use_of_protected_member
-                                      ? controller.companies.value
-                                          .take(300)
-                                          .map((e) => [
-                                                e.name,
-                                                e.cnpj,
-                                                e.pep,
-                                                e.ordinary,
-                                                e.totalAut,
-                                                e.totalDec,
-                                                e.county,
-                                                e.industry,
-                                              ])
-                                          .toList()
-                                      : controller.companyModelSearchRx.value
-                                          .take(300)
-                                          .map((e) => [
-                                                e.name,
-                                                e.cnpj,
-                                                e.pep,
-                                                e.ordinary,
-                                                e.totalAut,
-                                                e.totalDec,
-                                                e.county,
-                                                e.industry,
-                                              ])
-                                          .toList(),
-                                  dateFirst: '',
-                                  dateLast: '',
-                                ),
+                          iconColor: Colors.white,
+                          onExpansionChanged: (isExpanded) => print(isExpanded),
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(Layout.defaultPadding),
+                              width: Get.width,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                    Layout.defaultBorderRadius),
+                                color: Colors.white.withOpacity(0.1),
                               ),
-                              Obx(
-                                () => SizedBox(
-                                  width: Get.width,
-                                  child: TableWidget(
-                                    columns: [
-                                      'NOME',
-                                      'CNPJ',
-                                      'PEP',
-                                      'ORDINÁRIO',
-                                      'TOTAL AUTUAÇÃO',
-                                      'TOTAL DECLARADO',
-                                      'MUNICÍPIO',
-                                      'INDÚSTRIA',
-                                    ],
-                                    rows: controller
-                                            .companyModelSearchRx.value.isEmpty
-                                        // ignore: invalid_use_of_protected_member
-                                        ? controller.companies.value
-                                        : controller.companyModelSearchRx.value,
-                                    onName: (name) {
-                                      controller.orderBy(name);
-                                    },
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Obx(
+                                    () => HeaderTableWidget(
+                                      title: 'PGE',
+                                      isShowPdf: true,
+                                      onSearch: (value) =>
+                                          controller.searchCompany(value),
+                                      data: controller.companyModelSearchRx
+                                              .value.isEmpty
+                                          // ignore: invalid_use_of_protected_member
+                                          ? controller.companies.value
+                                              .take(300)
+                                              .map((e) => [
+                                                    e.name,
+                                                    e.cnpj,
+                                                    e.pep,
+                                                    e.ordinary,
+                                                    e.totalAut,
+                                                    e.totalDec,
+                                                    e.county,
+                                                    e.industry,
+                                                  ])
+                                              .toList()
+                                          : controller
+                                              .companyModelSearchRx.value
+                                              .take(300)
+                                              .map((e) => [
+                                                    e.name,
+                                                    e.cnpj,
+                                                    e.pep,
+                                                    e.ordinary,
+                                                    e.totalAut,
+                                                    e.totalDec,
+                                                    e.county,
+                                                    e.industry,
+                                                  ])
+                                              .toList(),
+                                      dateFirst: '',
+                                      dateLast: '',
+                                    ),
                                   ),
-                                ),
-                              )
-                            ],
-                          ),
+                                  Obx(
+                                    () => SizedBox(
+                                      width: Get.width,
+                                      child: TableWidget(
+                                        columns: [
+                                          'NOME',
+                                          'CNPJ',
+                                          'PEP',
+                                          'ORDINÁRIO',
+                                          'TOTAL AUTUAÇÃO',
+                                          'TOTAL DECLARADO',
+                                          'MUNICÍPIO',
+                                          'INDÚSTRIA',
+                                        ],
+                                        rows: controller.companyModelSearchRx
+                                                .value.isEmpty
+                                            // ignore: invalid_use_of_protected_member
+                                            ? controller.companies.value
+                                            : controller
+                                                .companyModelSearchRx.value,
+                                        onName: (name) {
+                                          controller.orderBy(name);
+                                        },
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                         withSpaceHeight(size: 20),
-                        Container(
-                          padding: EdgeInsets.all(Layout.defaultPadding),
-                          width: Get.width,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                                Layout.defaultBorderRadius),
-                            color: Colors.white.withOpacity(0.1),
+                        ExpansionTile(
+                          title: Text(
+                            '',
+                            style: TextStyle(color: Colors.white),
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Obx(
-                                () => HeaderTableWidget(
-                                  title: 'EPAT',
-                                  isShowPdf: true,
-                                  isShowFilter: true,
-                                  onFilter: (start, end) =>
-                                      controller.getEpatsOnFilter(start, end),
-                                  onSearch: (value) =>
-                                      controller.searchEpat(value),
-                                  data: controller
-                                          .epatModelSearchRx.value.isEmpty
-                                      // ignore: invalid_use_of_protected_member
-                                      ? controller.epats.value
-                                          .map((e) => [
-                                                e.aiim,
-                                                e.drt,
-                                                e.autuado,
-                                                e.data,
-                                              ])
-                                          .toList()
-                                      : controller.epatModelSearchRx.value
-                                          .map((e) => [
-                                                e.aiim,
-                                                e.drt,
-                                                e.autuado,
-                                                e.data,
-                                              ])
-                                          .toList(),
-                                  dateLast: controller.endDate.value,
-                                  dateFirst: controller.startDate.value,
-                                ),
+                          iconColor: Colors.white,
+                          onExpansionChanged: (isExpanded) => print(isExpanded),
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(Layout.defaultPadding),
+                              width: Get.width,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                    Layout.defaultBorderRadius),
+                                color: Colors.white.withOpacity(0.1),
                               ),
-                              Obx(
-                                () => Visibility(
-                                  visible: controller.resetar.value,
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        'Filtrando de ${controller.startDate} a ${controller.endDate}',
-                                        style: TextStyle(color: Colors.white),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Obx(
+                                    () => HeaderTableWidget(
+                                      title: 'EPAT',
+                                      isShowPdf: true,
+                                      isShowFilter: true,
+                                      onFilter: (start, end) => controller
+                                          .getEpatsOnFilter(start, end),
+                                      onSearch: (value) =>
+                                          controller.searchEpat(value),
+                                      data: controller
+                                              .epatModelSearchRx.value.isEmpty
+                                          // ignore: invalid_use_of_protected_member
+                                          ? controller.epats.value
+                                              .map((e) => [
+                                                    e.aiim,
+                                                    e.drt,
+                                                    e.autuado,
+                                                    e.data,
+                                                  ])
+                                              .toList()
+                                          : controller.epatModelSearchRx.value
+                                              .map((e) => [
+                                                    e.aiim,
+                                                    e.drt,
+                                                    e.autuado,
+                                                    e.data,
+                                                  ])
+                                              .toList(),
+                                      dateLast: controller.endDate.value,
+                                      dateFirst: controller.startDate.value,
+                                    ),
+                                  ),
+                                  Obx(
+                                    () => Visibility(
+                                      visible: controller.resetar.value,
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            'Filtrando de ${controller.startDate} a ${controller.endDate}',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          SizedBox(width: 40),
+                                          TextButton(
+                                              onPressed: () =>
+                                                  controller.resetarDate(),
+                                              child: Text('Resetar filtro'))
+                                        ],
                                       ),
-                                      SizedBox(width: 40),
-                                      TextButton(
-                                          onPressed: () =>
-                                              controller.resetarDate(),
-                                          child: Text('Resetar filtro'))
-                                    ],
+                                    ),
                                   ),
-                                ),
+                                  SizedBox(height: 20),
+                                  Obx(
+                                    () => SizedBox(
+                                      width: Get.width,
+                                      child: TableEpatWidget(
+                                        columns: [
+                                          'AIIM',
+                                          'DRT',
+                                          'AUTUADO',
+                                          'DATA AIIM',
+                                          'ASSUNTO',
+                                        ],
+                                        rows: controller
+                                                .epatModelSearchRx.value.isEmpty
+                                            // ignore: invalid_use_of_protected_member
+                                            ? controller.epats.value
+                                            : controller
+                                                .epatModelSearchRx.value,
+                                        onName: (name) {
+                                          controller.orderByEpat(name);
+                                        },
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
-                              SizedBox(height: 20),
-                              Obx(
-                                () => SizedBox(
-                                  width: Get.width,
-                                  child: TableEpatWidget(
-                                    columns: [
-                                      'AIIM',
-                                      'DRT',
-                                      'AUTUADO',
-                                      'DATA AIIM',
-                                      'ASSUNTO',
-                                    ],
-                                    rows: controller
-                                            .epatModelSearchRx.value.isEmpty
-                                        // ignore: invalid_use_of_protected_member
-                                        ? controller.epats.value
-                                        : controller.epatModelSearchRx.value,
-                                    onName: (name) {
-                                      controller.orderByEpat(name);
-                                    },
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
+                            )
+                          ],
                         )
                       ],
                     ),
